94076e881f011b07e607d9b9360ad5ac4778b54d
cz2925928087 <2925928087@qq.com>
2026-09-21 14:51:34 +0800

    后端：新增竞赛与竞赛题目关系表建表语句

--- alog-arena-oj/deploy/init.sql
+++ alog-arena-oj/deploy/init.sql
@@ -38,3 +38,39 @@ create table 'tb_question' (
     `update_time` datetime not null comment '更新时间',
     primary key (`question_id`)
 );
+
+-- 竞赛管理:
+-- B端: 列表,新增,编辑,删除,发布,撤销发布
+
+-- C端:两个列表(未完赛,历史 ),报名参赛,参加竞赛(竞赛倒计时,完成竞赛,竞赛内题目切换),竞赛练习,查看排名,我的比赛,我的消息,
+
+-- 是否开赛可以根据开赛时间实时计算,不需要加载到数据库
+-- 报名参赛: 未开始,未报名
+create table if not exists tb_exam
+(
+    `exam_id`     bigint unsigned not null comment '竞赛id',
+    `title`       varchar(50)     not null comment '竞赛标题',
+    `start_time`  datetime        not null comment '竞赛开始时间',
+    `end_time`    datetime        not null comment '竞赛结束时间',
+    `status`      tinyint         not null default '0' comment '发布状态 0:未发布 1:已发布,默认为0:未发布',
+    `create_by`   bigint          not null comment '创建人',
+    `create_time` datetime        not null comment '创建时间',
+    `update_by`   bigint          not null comment '更新用户',
+    `update_time` datetime        not null comment '更新时间',
+    primary key (exam_id)
+);
+
+-- 题目和竞赛之间是多对一的关系
+-- 题目和竞赛的关系表
+create table if not exists tb_exam_question
+(
+    `exam_question_id` bigint unsigned not null comment '竞赛题目关系id(主键)',
+    `question_id`      bigint unsigned not null comment '题目id(主键)',
+    `exam_id`          bigint unsigned not null comment '竞赛id(主键)',
+    `question_order`   bigint          not null comment '题目顺序',
+    `create_by`        bigint          not null comment '创建人',
+    `create_time`      datetime        not null comment '创建时间',
+    `update_by`        bigint          not null comment '更新用户',
+    `update_time`      datetime        not null comment '更新时间',
+    primary key (exam_question_id)
+);
